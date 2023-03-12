2a5ce727a98cfa1788ea7d09a215a11beb12d291
blanho <h.baolan20025@gmail.com>
2023-03-12 17:01:44 +0700

    feat: implement header and footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
-import "./App.css";
-
+import Header from "./components/layout/Header";
+import Footer from "./components/layout/Footer";
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <Header />
+      <Footer />
     </div>
   );
 }
